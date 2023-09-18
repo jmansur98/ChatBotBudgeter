308055c4a3acacb3f673b60779b9c759d399d0ed
juanmansur98 <juancruzmansur@hotmail.com>
2023-09-18 20:55:50 +0200

    PresupuestoChatBot1.0

--- js/index.js
+++ js/index.js
@@ -1,48 +1,43 @@
-
 function calcularPresupuesto() {
-  const opcionesSeleccionadasSeccion1 = [];
-  const checkboxesSeccion1 = document.querySelectorAll('#seccion1 input[type="checkbox"]');
-  checkboxesSeccion1.forEach(checkbox => {
-      if (checkbox.checked) {
-          opcionesSeleccionadasSeccion1.push(checkbox.value);
-      }
-  });
-  const opcionesSeleccionadasSeccion2 = [];
-  const checkboxesSeccion2 = document.querySelectorAll('#seccion2 input[type="checkbox"]');
-  checkboxesSeccion2.forEach(checkbox => {
-      if (checkbox.checked) {
-          opcionesSeleccionadasSeccion2.push(checkbox.value);
-      }
-  });
+    const nombreUsuario = prompt("Por favor, ingresa tu nombre:");
+    // opciones seleccionadas
+    const opcionesSeleccionadas = [];
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+    for (const checkbox of checkboxes) {
+        opcionesSeleccionadas.push(checkbox.value);
+    }
 
-  // Calcular el monto total del presupuesto combinando las secciones.
-  let montoTotal = 0;
-  opcionesSeleccionadasSeccion1.concat(opcionesSeleccionadasSeccion2).forEach(opcion => {
-      switch (opcion) {
-          case 'Opción 1':
-              montoTotal += 100;
-              break;
-          case 'Opción 2':
-              montoTotal += 150;
-              break;
-          case 'Opción 3':
-              montoTotal += 200;
-              break;
-          case 'Opción 4':
-              montoTotal += 50;
-              break;
-          case 'Opción 5':
-              montoTotal += 75;
-              break;
-              case 'Opción 6':
+    // Calcular el monto total
+    let montoTotal = 0;
+    for (const opcion of opcionesSeleccionadas) {
+        switch (opcion) {
+            case 'Opción 1':
+                montoTotal += 100;
+                break;
+            case 'Opción 2':
+                montoTotal += 150;
+                break;
+            case 'Opción 3':
+                montoTotal += 200;
+                break;
+            case 'Opción 4':
+                montoTotal += 50;
+                break;
+            case 'Opción 5':
+                montoTotal += 75;
+                break;
+            case 'Opción 6':
                 montoTotal += 120;
                 break;
             default:
                 break;
-      }
-  });
-  // Mostrar resultato total debajo de la pag. 
-  const resultadoElement = document.getElementById('resultado');
-  resultadoElement.textContent = `Monto Total del Presupuesto: $${montoTotal}`;
+        }
+    }
+
+    // Mostrar el resultado mediante  un alert()
+    alert(`${nombreUsuario}: el monto total del presupuesto es: €${montoTotal}`);
 }
-        
+
+const calcularBtn = document.getElementById('calcularBtn');
+calcularBtn.addEventListener('click', calcularPresupuesto);
+
