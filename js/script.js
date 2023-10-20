265a6b5d5c081c4b5775b2890733b5f5fabeb86c
juanmansur98 <juancruzmansur@hotmail.com>
2023-10-20 18:15:37 +0200

    PresupuestarioChatbot2.0

---
+++ js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    const content = document.querySelector('.container'); 
+    loader.style.display = 'none';
+    content.style.display = 'block'; 
+});
